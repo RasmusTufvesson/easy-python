ebaa898aeab2d3d011f56ffa88a7ff15d879fe34
RasmusTufvesson <lars@bizzbook.com>
2021-01-25 20:56:39 +0100

    added buttons

--- pgez/classes.py
+++ pgez/classes.py
@@ -44,6 +44,9 @@ class Object:
         box = self.image.get().get_rect()
         box.center = self.pos.get()
         return self.image.get(), box
+    
+    def change_pos(self, new_pos):
+        self.pos = new_pos
 
 class AnimatedObject(Object):
     def __init__(self, dir_path: str, pos: Vector2 = Vector2.ZERO, frames_between_animation_tick: int = 0):
@@ -125,6 +128,41 @@ class Text:
         box.center = self.pos.get()
         return self.image.get, box
 
+class EventListener:
+    def __init__(self, event, function):
+        self.event = event
+        self.result = function
+    
+    def tick(self):
+        if self.event():
+            self.result()
+
+
+
+class Button(EventListener, Object):
+    def __init__(self, image: Image, press_function, pos: Vector2 = Vector2.ZERO, cooldown = 2):
+        Object.__init__(self, image, pos)#print (list(super()))
+        EventListener.__init__(self, self.event_check, press_function)
+        self.box = image.get().get_rect()
+        self.box.center = pos
+        self.cooldown = cooldown
+        self.max_cooldown = cooldown
+    
+    def tick(self):
+        self.cooldown -= 1
+        if self.cooldown < 1:
+            self.cooldown = self.max_cooldown
+            if self.event():
+                self.result()
+    
+    def change_pos(self, new_pos):
+        self.pos = new_pos
+        self.box.center = self.pos
+    
+    def event_check(self):
+        mouse = pygame.mouse.get_pos()
+        return self.box.collidepoint(mouse)
+
 class App:
     #disabled = False
     def __init__(self, title: str, dimensions: tuple, background: Color = None, speed: int = 60, icon: Image = None):
@@ -136,10 +174,12 @@ class App:
         self.clock = pygame.time.Clock()
         self.speed = speed
         self.object_array = []
+        self.event_listener_array = []
         self.background = background
         self.title = title
         self.icon = icon
         self.disabled = False
+        self.dimensions = dimensions
 
     def tick(self):
         if not self.disabled:
@@ -151,6 +191,7 @@ class App:
                     #print ("shuting down")
             
             self._display_all()
+            self._listen_for_events()
 
             self.clock.tick(self.speed)
         #else:
@@ -166,17 +207,28 @@ class App:
                 #box.center = (100, 100)
                 #self.screen.blit(pygame.image.load("D:/Rasmus/python/chicken game/art/chicken_l_golden.png"), box)#print ("doing blit")
         pygame.display.update()
+    
+    def _listen_for_events(self):
+        for listener in self.event_listener_array:
+            listener.tick()
 
-    def add_object(self, obj: Object):
+    def add_object(self, obj):
         self.object_array.append(obj)
-        print (self.object_array)
+        #print (self.object_array)
+    
+    def add_event_listener(self, listener):
+        self.event_listener_array.append(listener)
     
     def run(self):
         return MainLoop(self).run()
     
     def reset(self):
-        pygame.init()
+        self.__init__(self.title, self.dimensions, self.background, self.speed, self.icon)
         self.disabled = False
+    
+    def disable(self):
+        self.disabled = True
+        pygame.quit()
 
 class MainLoop:
     def __init__(self, app: App):
@@ -188,7 +240,7 @@ class MainLoop:
             #print ("tick!")
 
 class UserSingleLineText(Text):
-    def await_user_input(self, app: App, text_change_call = self.change_text, cooldown = 0.1):
+    def await_user_input(self, app: App, text_change_call = lambda text, key: self.change_text(text, key), cooldown = 0.1):
         def key_await():
             key_gotten = False
             while not key_gotten:
